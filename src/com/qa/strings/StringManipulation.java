0079e14a1c1c7fed5e163ab333b2241c087e5a7e
juan <juanbonami@gmail.com>
2021-05-18 11:32:21 -0500

    used to substrings to join them to reach desired output

--- src/com/qa/strings/StringManipulation.java
+++ src/com/qa/strings/StringManipulation.java
@@ -3,13 +3,17 @@ package com.qa.strings;
 public class StringManipulation {
 	
 	 static String firstStr = "yesterday it was raining";
-	static String secondStr = "today it is sunny";
+	 static String secondStr = "today it is sunny";
 	
 	 static String newStr = secondStr + ", " + firstStr;
+	 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(newStr.toUpperCase());
+		System.out.println(newStr.toUpperCase()); // output: TODAY IT IS SUNNY, YESTERDAY IT WAS RAINING
+		
+		System.out.println(newStr.toUpperCase().substring(0, 11) + " " + newStr.toUpperCase().substring(36, 43));
+		// output: TODAY IT IS RAINING
 	}
 
 }
