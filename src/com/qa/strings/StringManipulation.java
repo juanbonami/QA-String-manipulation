77a8c00d1e7c5160e481f68eca7aaa7b04c3bab2
juan <juanbonami@gmail.com>
2021-05-18 11:22:15 -0500

    conatinating string

---
+++ src/com/qa/strings/StringManipulation.java
@@ -0,0 +1,15 @@
+package com.qa.strings;
+
+public class StringManipulation {
+	
+	 static String firstStr = "yesterday it was raining";
+	static String secondStr = "today it is sunny";
+	
+	 static String newStr = secondStr + ", " + firstStr;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(newStr.toUpperCase());
+	}
+
+}
